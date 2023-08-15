7d482d514a4549ae65e10fc3602dbfb2049c7d72
Fernando <tukori2@gmail.com>
2023-08-14 22:39:35 -0300

    minimax

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,47 +1,121 @@
 package org.example;
 
-import io.github.asdfjkl.jchesslib.Board;
-import io.github.asdfjkl.jchesslib.Game;
-import io.github.asdfjkl.jchesslib.Move;
+import io.github.asdfjkl.jchesslib.*;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Timer;
 
 public class Main {
+    // turn = 0 -> white
+    // turn = 1 -> black
+    private static int WHITE_DEPTH = 3;
+    private static int BLACK_DEPTH = 1;
+
     public static void main(String[] args) {
+        String testFen = "6kr/8/8/8/8/8/8/RK6 w - - 0 1";
         Board board = new Board(true);
-        Game game = new Game();
-        // turn = 0 -> white
-        // turn = 1 -> black
-        ArrayList<Move> legalMoves = board.legalMoves();
-        Node rootNode = new Node(board, null, null);
-        for (Move legalMove : legalMoves) {
-            Board clonedBoard = board.makeCopy();
-            clonedBoard.apply(legalMove);
-            Node node = new Node(clonedBoard, rootNode, legalMove);
-            rootNode.addNodeAsChild(node);
-        }
 
-
-        Node maxUCBChild = rootNode.getMaxUCBChild();
-        Node expansion = expansion(maxUCBChild);
-        System.out.println("Main.main");
-    }
-
-    public static Node expansion(Node node) {
-        if (node.getChildren().size() == 0) {
-            //nó folha
-            return node;
-        }
-        double maxUCB = Double.MIN_VALUE;
-        Node selectedChild = null;
-        for (Node child : node.getChildren()) {
-            double currentUCB = child.getUcb().getValue();
-            if (currentUCB > maxUCB) {
-                maxUCB = currentUCB;
-                selectedChild = child;
+        while (!gameEndingConditions(board)) {
+            int depth = board.turn == 0 ? WHITE_DEPTH : BLACK_DEPTH;
+            Node rootNode = new Node(board, null, null, 0);
+            initializeTree(rootNode, depth, 0);
+            Move move = getMinimaxMove(rootNode, depth);
+//            System.out.println("\t\tboard turn " +  board.turn + "\t\tdepth:" + depth);
+            if (move != null) {
+                board.apply(move);
+                System.out.println(board);
+                System.out.println(board.fen());
             }
         }
-        return expansion(selectedChild);
+        System.out.println("-------------------");
+        System.out.println("ending fen:" + board.fen());
+    }
+
+    public static boolean gameEndingConditions(Board board) {
+        if (board.isCheckmate()) {
+            System.out.println("Checkmate");
+            return true;
+        } else if (board.isStalemate()) {
+            System.out.println("Stalemate");
+            return true;
+        } else if (board.isInsufficientMaterial()) {
+            System.out.println("Insufficient Material");
+            return true;
+        }
+        return false;
+    }
+
+    public static Move getMinimaxMove(Node rootNode, int depth) {
+        ArrayList<Node> moveCandidates = new ArrayList<>();
+        Instant start = Instant.now();
+        int minimax = minimax(rootNode, depth, Integer.MIN_VALUE, Integer.MAX_VALUE, rootNode.getBoard().turn);
+        Instant end = Instant.now();
+        System.out.println(Duration.between(start, end).toMillis() + "ms");
+        for (Node child : rootNode.getChildren()) {
+            if (child.getValue() == minimax) {
+                moveCandidates.add(child);
+            }
+        }
+        if (moveCandidates.size() == 1) {
+            return moveCandidates.get(0).getMove();
+        } else if (moveCandidates.size() == 0) {
+            return null;
+        } else {
+            int randomIndex = (int) Math.floor(Math.random() * moveCandidates.size());
+            return moveCandidates.get(randomIndex).getMove();
+        }
+    }
+
+    public static void initializeTree(Node parentNode, int depth, int nodeDepth) {
+        if (depth > 0) {
+            Board parentBoard = parentNode.getBoard();
+            ArrayList<Move> legalMoves = parentBoard.legalMoves();
+            for (Move legalMove : legalMoves) {
+                Board clonedBoard = parentBoard.makeCopy();
+                clonedBoard.apply(legalMove);
+                Node node = new Node(clonedBoard, parentNode, legalMove, nodeDepth + 1);
+                parentNode.addNodeAsChild(node);
+                initializeTree(node, depth - 1, nodeDepth + 1);
+            }
+        }
+    }
+
+
+    //maximizar -> white
+    //minimizar -> black
+    public static int minimax(Node node, int depth, int alpha, int beta, int playerTurn) {
+        if (node.getChildren().size() == 0 || depth == 0) {
+            return node.getValue();
+        }
+
+        if (playerTurn == 0) {
+            int maxValue = Integer.MIN_VALUE;
+            for (Node child : node.getChildren()) {
+                int value = minimax(child, depth - 1, alpha, beta, 1);
+                maxValue = Math.max(maxValue, value);
+                alpha = Math.max(alpha, maxValue);
+                if (alpha >= beta) {
+                    break; // beta
+                }
+            }
+            node.setValue(maxValue); // Set the value for the current node
+            return maxValue;
+        } else {
+            int minValue = Integer.MAX_VALUE;
+            for (Node child : node.getChildren()) {
+                int value = minimax(child, depth - 1, alpha, beta, 0);
+                minValue = Math.min(minValue, value);
+                beta = Math.min(beta, minValue);
+                if (alpha >= beta) {
+                    break; // alpha
+                }
+            }
+            node.setValue(minValue); // Set the value for the current node
+            return minValue;
+        }
 
     }
 
@@ -67,7 +141,7 @@ public class Main {
         return x == 0 ? "WHITE" : "BLACK";
     }
 
-    public static void checkmate(Board board){
+    public static void checkmate(Board board) {
         if (!board.isCheckmate()) {
             //isCheckmate -> returns true if you checkmate the opponent's king (win)
             int kingPos = board.getKingPos(0);
--- src/main/java/org/example/Node.java
+++ src/main/java/org/example/Node.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import io.github.asdfjkl.jchesslib.Board;
+import io.github.asdfjkl.jchesslib.CONSTANTS;
 import io.github.asdfjkl.jchesslib.Move;
 
 import java.util.ArrayList;
@@ -10,20 +11,24 @@ public class Node {
     private Board board;
     private Node parent;
     private Move move;
-    private UCB ucb;
     private ArrayList<Node> children = new ArrayList<>();
-    private double visitCount;
 
-    public Node(Board board, Node parentNode, Move legalMove){
+    private int nodeDepth;
+
+    private int value;
+    private int playerColor;
+    // turn = 0 -> white
+    // turn = 1 -> black
+
+    public Node(Board board, Node parentNode, Move legalMove, int nodeDepth) {
         this.board = board;
-        this.ucb = new UCB();
-        this.ucb.updateValue(1, 0);
         this.parent = parentNode;
         this.move = legalMove;
-
+        this.nodeDepth = nodeDepth;
+        this.value = evaluate();
     }
 
-    public void addNodeAsChild(Node child){
+    public void addNodeAsChild(Node child) {
         this.children.add(child);
     }
 
@@ -40,27 +45,52 @@ public class Node {
         return move;
     }
 
-    public UCB getUcb() {
-        return ucb;
-    }
 
     public ArrayList<Node> getChildren() {
         return children;
     }
 
-    public double getVisitCount() {
-        return visitCount;
-    }
 
-    public Node getMaxUCBChild() {
-        double maxUCB = Double.MIN_VALUE;
-        Node maxUCBChild = null;
-        for (Node child : this.children) {
-            if (child.ucb.getValue() > maxUCB){
-                maxUCB = child.ucb.getValue();
-                maxUCBChild = child;
+    public int evaluate() {
+        int blackScore = 0;
+        int whiteScore = 0;
+        int[] pieceValues = {1, 3, 3, 5, 9, 0};
+        //  EMPTY, PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING
+
+        for (int i = 21; i < 99; i++) {
+            int piece = this.board.getPieceAt(i);
+
+            if (piece != CONSTANTS.EMPTY) {
+                if (piece >= CONSTANTS.BLACK_PAWN && piece <= CONSTANTS.BLACK_KING) {
+//                    System.out.println("black piece");
+                    blackScore += pieceValues[piece - CONSTANTS.BLACK_PAWN];
+                } else if (piece >= CONSTANTS.WHITE_PAWN && piece <= CONSTANTS.WHITE_KING) {
+//                    System.out.println("white piece");
+                    whiteScore += pieceValues[piece - CONSTANTS.WHITE_PAWN];
+                }
             }
         }
-        return maxUCBChild;
+//        return playerColor == 0 ? whiteScore : blackScore;
+        return whiteScore - blackScore;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getPlayerColor() {
+        return playerColor;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "nodeDepth=" + nodeDepth +
+                ", value=" + value +
+                '}';
     }
 }
